49676b924551378d27ad8c36bdea659995ca35a9
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-05-29 18:50:38 +0530

    Implement car-detail page design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/General/Navbar'
 import Home from './components/Home'
 import AllCars from './components/Cars/AllCars'
+import CardDetails from './components/Cars/CarDetails'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cars" element={<AllCars />} />
+        <Route path="/car-details" element={<CardDetails />} />
       </Routes>
     </>
   )
