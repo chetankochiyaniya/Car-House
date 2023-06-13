15fb6b5df5e6ec720a04e5d3345462fffc128e32
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-06-13 16:20:11 +0530

    Dashboard: car list table design

--- src/Dashboard/AdminCars/index.js
+++ src/Dashboard/AdminCars/index.js
@@ -1,13 +1,97 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import MUIDataTable from 'mui-datatables'
 import SideBar from '../SideBar'
+import { useDispatch, useSelector } from 'react-redux'
+import './index.css'
+import { getCarDetails } from '../../redux/actions'
+import { CircularProgress } from '@mui/material'
+const AdminCars = () => {
+  const { data, isLoading, error } = useSelector((state) => state.fetchDataReducer)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getCarDetails())
+  }, [dispatch])
 
-function AdminCars() {
+  if (isLoading) {
+    return <CircularProgress color="error" />
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+
+  const rows = data.map((car) => {
+    return [
+      car.car_id,
+      car.price.originalPrice,
+      car.price.discountedPrice,
+      car.carName,
+      car.description
+    ]
+  })
+  const columns = [
+    {
+      name: 'Id',
+      options: {
+        filter: true
+      }
+    },
+    {
+      name: 'Original Price',
+      options: {
+        filter: true,
+        sort: true
+      }
+    },
+    {
+      name: 'Discounted Price',
+      options: {
+        filter: true
+      }
+    },
+    {
+      name: 'Car Name',
+      options: {
+        filter: true
+      }
+    },
+    {
+      name: 'Description',
+      options: {
+        filter: true
+      }
+    },
+    {
+      name: 'Action',
+      options: {
+        filter: false,
+        sort: false,
+        empty: true,
+        customBodyRender: () => {
+          return (
+            <>
+              <div className="d-flex">
+                <button className="custom-table-btn">Edit</button>
+                <button className="custom-table-btn">Delete</button>
+              </div>
+            </>
+          )
+        }
+      }
+    }
+  ]
+
+  const options = {
+    filter: true,
+    filterType: 'dropdown',
+    page: 0,
+    selectableRows: 'none'
+  }
+
   return (
-    <>
-      <SideBar>
-        <h1>Admin cars</h1>
-      </SideBar>
-    </>
+    <SideBar>
+      <MUIDataTable title={'Cars List'} data={rows} columns={columns} options={options} />
+    </SideBar>
   )
 }
 
--- src/Dashboard/AdminChatSupport/index.js
+++ src/Dashboard/AdminChatSupport/index.js
@@ -1,4 +1,3 @@
-import { Container } from '@mui/system'
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
 import SideBar from '../SideBar'
@@ -8,14 +7,12 @@ const AdminChatSupport = () => {
   return (
     <>
       <SideBar>
-        <Container sx={{ marginTop: '50px' }}>
-          <ChatEngine
-            height="80vh"
-            projectID="08b634e6-f6e7-479c-a9b2-730d5ddb76cd"
-            userName="Chetan_Kochiyaniya"
-            userSecret="Chetan@2020"
-          />
-        </Container>
+        <ChatEngine
+          height="75vh"
+          projectID="08b634e6-f6e7-479c-a9b2-730d5ddb76cd"
+          userName="Chetan_Kochiyaniya"
+          userSecret="Chetan@2020"
+        />
       </SideBar>
     </>
   )
